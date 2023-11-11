b1129fa69fe43a1a9821cd3882b05aef23f15008
ishtvankomka <ishtvan.komonii.it.2021@lpnu.ua>
2023-11-11 19:48:28 +0200

    Fix: issue with > 64 bytes text

--- src/services/desxCrypto.ts
+++ src/services/desxCrypto.ts
@@ -1,28 +1,45 @@
 import { feistelRound, initialPermutation, inverseInitialPermutation, whitening, xorArrays } from "../helpers/crypto";
 
 export const desx = (bytes: number[], keyBytes: number[], tweakBytes: number[]): number[] => {
-    // 1. Key Whitening
-    let keyWhitened = whitening(keyBytes, tweakBytes);
+    // Break the input bytes into 64 byte blocks (function not )
+    const blocks = splitArrayIntoBlocks(bytes, 64);
 
-    // 2. Initial Permutation (IP)
-    let permutedText = initialPermutation(bytes);
+    // Encrypt each block separately
+    const encryptedBlocks = blocks.map((block) => {
+        // 1. Key Whitening
+        let keyWhitened = whitening(keyBytes, tweakBytes);
 
-    // 3. Rounds of Feistel Network
-    for (let round = 0; round < 16; round++) {
-        const leftHalf = permutedText.slice(0, 32);
-        const rightHalf = permutedText.slice(32);
-        const roundKey = keyWhitened.slice(0, 48);
+        // 2. Initial Permutation (IP)
+        let permutedText = initialPermutation(block);
 
-        const newRightHalf = feistelRound(rightHalf, roundKey);
+        // 3. Rounds of Feistel Network
+        for (let round = 0; round < 16; round++) {
+            const leftHalf = permutedText.slice(0, 32);
+            const rightHalf = permutedText.slice(32);
+            const roundKey = keyWhitened.slice(0, 48);
 
-        // Update for the next round
-        keyWhitened = xorArrays(leftHalf, rightHalf);
-        permutedText = leftHalf.concat(newRightHalf);
+            const newRightHalf = feistelRound(rightHalf, roundKey);
+
+            // Update for the next round
+            keyWhitened = xorArrays(leftHalf, rightHalf);
+            permutedText = leftHalf.concat(newRightHalf);
+        }
+
+        // 4. Inverse Initial Permutation (IP^(-1))
+        const encryptedText = inverseInitialPermutation(permutedText);
+
+        // 5. Final Whitening
+        return whitening(encryptedText, tweakBytes);
+    });
+
+    // Concatenate the encrypted blocks back into a single array
+    return encryptedBlocks.reduce((acc, block) => acc.concat(block), []);
+};
+
+const splitArrayIntoBlocks = (array: number[], blockSize: number): number[][] => {
+    const blocks = [];
+    for (let i = 0; i < array.length; i += blockSize) {
+        blocks.push(array.slice(i, i + blockSize));
     }
-
-    // 4. Inverse Initial Permutation (IP^(-1))
-    const encryptedText = inverseInitialPermutation(permutedText);
-
-    // 5. Final Whitening
-    return whitening(encryptedText, tweakBytes);
+    return blocks;
 };
